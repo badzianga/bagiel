800ba12a2e5122c62526e4a75d072cacfef89e1a
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-01-15 15:58:48 +0100

    Added basic Vector2 class

---
+++ include/Vector2.hpp
@@ -0,0 +1,17 @@
+#ifndef BAGIEL_VECTOR2_HPP
+#define BAGIEL_VECTOR2_HPP
+
+namespace bgl {
+
+    class Vector2 {
+    public:
+        float x;
+        float y;
+
+        Vector2();
+        Vector2(float X, float Y);
+    };
+
+} // bgl
+
+#endif //BAGIEL_VECTOR2_HPP
---
+++ src/Vector2.cpp
@@ -0,0 +1,9 @@
+#include <Vector2.hpp>
+
+namespace bgl {
+
+    Vector2::Vector2() : x(0.f), y(0.f) {}
+
+    Vector2::Vector2(float X, float Y) : x(X), y(Y) {}
+
+} // bgl
