6d145dfb5b5a526e2b65b4f8faa95214d757810b
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-01-15 18:36:51 +0100

    Added Shader class for rendering

---
+++ include/Shader.hpp
@@ -0,0 +1,34 @@
+#ifndef BAGIEL_SHADER_HPP
+#define BAGIEL_SHADER_HPP
+
+#include <cstdint>
+#include <unordered_map>
+#include <string>
+
+namespace bgl {
+
+    class Shader {
+    public:
+        Shader();
+        Shader(const char* vertexPath, const char* fragmentPath);
+        ~Shader();
+        void use() const;
+        void compile(const char* vertexPath, const char* fragmentPath);
+        void setBool(const char* name, bool value);
+        void setInt(const char* name, int value);
+        void setFloat(const char* name, float value);
+        void setVector2(const char* name, float x, float y);
+        void setVector3(const char* name, float x, float y, float z);
+        void setVector4(const char* name, float x, float y, float z, float w);
+    private:
+        uint32_t m_id;
+        std::unordered_map<const char*, int32_t> m_uniformLocationCache;
+
+        std::string readFile(const char* path);
+        int32_t getUniformLocation(const char* name);
+        void checkCompileErrors(uint32_t shaderID, const std::string& type);
+    };
+
+} // bgl
+
+#endif //BAGIEL_SHADER_HPP
---
+++ src/Shader.cpp
@@ -0,0 +1,124 @@
+#include <Shader.hpp>
+
+#include <glad/glad.h>
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+namespace bgl {
+
+    Shader::Shader() {
+        m_id = glCreateProgram();
+    }
+
+    Shader::Shader(const char* vertexPath, const char* fragmentPath) {
+        m_id = glCreateProgram();
+        compile(vertexPath, fragmentPath);
+    }
+
+    Shader::~Shader() {
+        glDeleteProgram(m_id);
+    }
+
+    void Shader::use() const {
+        glUseProgram(m_id);
+    }
+
+    void Shader::compile(const char* vertexPath, const char* fragmentPath) {
+        std::string vertexShaderString = readFile(vertexPath);
+        const char* vertexShaderSource = vertexShaderString.c_str();
+        uint32_t vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+        glCompileShader(vertexShader);
+        checkCompileErrors(vertexShader, "VERTEX");
+
+        std::string fragmentShaderString = readFile(fragmentPath);
+        const char* fragmentShaderSource = fragmentShaderString.c_str();
+        uint32_t fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+        glCompileShader(fragmentShader);
+        checkCompileErrors(fragmentShader, "FRAGMENT");
+
+        glAttachShader(m_id, vertexShader);
+        glAttachShader(m_id, fragmentShader);
+        glLinkProgram(m_id);
+        checkCompileErrors(m_id, "PROGRAM");
+
+        glUseProgram(m_id);
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+    }
+
+    void Shader::setBool(const char* name, bool value) {
+        glUniform1i(getUniformLocation(name), (int)value);
+    }
+
+    void Shader::setInt(const char* name, int value) {
+        glUniform1i(getUniformLocation(name), value);
+    }
+
+    void Shader::setFloat(const char* name, float value) {
+        glUniform1f(getUniformLocation(name), value);
+    }
+
+    void Shader::setVector2(const char* name, float x, float y) {
+        glUniform2f(getUniformLocation(name), x, y);
+    }
+
+    void Shader::setVector3(const char* name, float x, float y, float z) {
+        glUniform3f(getUniformLocation(name), x, y, z);
+    }
+
+    void Shader::setVector4(const char* name, float x, float y, float z, float w) {
+        glUniform4f(getUniformLocation(name), x, y, z, w);
+    }
+
+    std::string Shader::readFile(const char* path) {
+        std::ifstream file;
+        file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        std::stringstream stream;
+        try {
+            file.open(path, std::ios::in);
+            stream << file.rdbuf();
+            file.close();
+        }
+        catch (std::ifstream::failure& error) {
+            std::cerr << "[SHADER ERROR] Shader file " << path << " not successfully read: " << error.what() << '\n';
+            exit(-1);
+        }
+        return stream.str();
+    }
+
+    int32_t Shader::getUniformLocation(const char* name) {
+        auto locationIterator = m_uniformLocationCache.find(name);
+        if (locationIterator != m_uniformLocationCache.end()) {
+            return locationIterator->second;
+        }
+        int32_t location = glGetUniformLocation(m_id, name);
+        m_uniformLocationCache[name] = location;
+        return location;
+    }
+
+    void Shader::checkCompileErrors(uint32_t shaderID, const std::string& type) {
+        int success;
+        char infoLog[1024];
+        if (type != "PROGRAM") {
+            glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
+            if (!success) {
+                glGetShaderInfoLog(shaderID, 1024, nullptr, infoLog);
+                std::cerr << "[SHADER ERROR] Shader compilation error of type: " << type << '\n' << infoLog << '\n';
+                exit(-1);
+            }
+        }
+        else {
+            glGetProgramiv(shaderID, GL_LINK_STATUS, &success);
+            if (!success) {
+                glGetProgramInfoLog(shaderID, 1024, nullptr, infoLog);
+                std::cerr << "[SHADER ERROR] Program linking error of type: " << type << '\n' << infoLog << '\n';
+                exit(-1);
+            }
+        }
+    }
+
+} // bgl
