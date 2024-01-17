20eb448979db0e3ffc4dcf7491570ef48656e370
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-01-17 14:33:42 +0100

    Added multiplication operator to Vector2

--- include/Vector2.hpp
+++ include/Vector2.hpp
@@ -36,7 +36,9 @@ namespace bgl {
     Vector2& operator-=(Vector2& v1, const Vector2& v2);
     Vector2 operator*(const Vector2& v, float f);
     Vector2 operator*(float f, const Vector2& v);
+    Vector2 operator*(const Vector2& v1, const Vector2& v2);
     Vector2& operator*=(Vector2& v, float f);
+    Vector2& operator*=(Vector2& v1, const Vector2& v2);
     Vector2 operator/(const Vector2& v, float f);
     Vector2 operator/(float f, const Vector2& v);
     Vector2& operator/=(Vector2& v, float f);
--- src/Vector2.cpp
+++ src/Vector2.cpp
@@ -72,10 +72,20 @@ namespace bgl {
         return { v.x * f, v.y * f };
     }
 
+    Vector2 operator*(const Vector2& v1, const Vector2& v2) {
+        return { v1.x * v2.x, v1.y * v2.y };
+    }
+
     Vector2& operator*=(Vector2& v, float f) {
         v.x *= f;
         v.y *= f;
         return v;
+    }
+
+    Vector2& operator*=(Vector2& v1, const Vector2& v2) {
+        v1.x *= v2.x;
+        v1.y *= v2.y;
+        return v1;
     }
 
     Vector2 operator/(const Vector2& v, float f) {
